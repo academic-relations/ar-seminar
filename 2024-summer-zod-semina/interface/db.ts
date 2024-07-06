29d210b597a4d9e164d2ed8bf309667485dbafb1
SeungJong Ha <hippo0419@kaist.ac.kr>
2024-07-06 23:46:08 +0900

    feat: add README

--- 2024-summer-zod-semina/interface/db.ts
+++ 2024-summer-zod-semina/interface/db.ts
@@ -1,7 +1,27 @@
 import { z } from "zod";
 
-// TODO1: 아래와 같이 2개의 양의 정수를 포함한 객체를 parsing하는 zod object를 구현하세요
-// { a: 양의 정수, b: 양의 정수 }
-const twoPositiveIntegerParser = undefined
+export const dbElement = z.object({
+  value: z.coerce.number().min(0),
+  updatedAt: z.coerce.date(),
+});
 
-export default twoPositiveIntegerParser
+export const twoPositiveIntegerParser = z.object({
+  // TODO1: 아래와 같이 2개의 양의 정수를 포함한 객체를 parsing하는 zod object를 구현하세요
+  // { a: 양의 정수, b: 양의 정수 }
+});
+
+export const getDbIndexParser = (dbLength: number) =>
+  z.object({
+    index: z.coerce
+      .number()
+      .int()
+      .min(0)
+      .max(dbLength - 1),
+  });
+
+export const dbPaginationQueryParser = z.object({
+  startDate: z.optional(z.coerce.date()),
+  endDate: z.optional(z.coerce.date()),
+  pageOffset: z.coerce.number().int().min(1),
+  itemCount: z.coerce.number().int().min(1),
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# SPARCS 2024 Summer Seminars
+2024학년도 여름 SPARCS 세미나의 예제 코드 및 과제를 정리하기 위한 레포지토리 입니다.
+
+## 2024 Summer TypeScript & Zod Seminar
