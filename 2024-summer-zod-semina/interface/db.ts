f358586b71727ca118e7d4df019d8b6b9d977f7d
oreo <oreo@sparcs.org>
2024-07-17 21:56:28 +0900

    complete zod HW

--- 2024-summer-zod-semina/interface/db.ts
+++ 2024-summer-zod-semina/interface/db.ts
@@ -1,19 +1,27 @@
+import { start } from "repl";
 import { z } from "zod";
 
 export const dbElement = z.object({
   // TODO 2: mockDB에 들어갈 데이터의 타입을 정의해 주세요
   // { value: 0 이샹의 정수, updatedAt: ts date 타입 }
+  value: z.coerce.number().int().nonnegative(),
+  updatedAt: z.coerce.date()
 });
 
 export const twoPositiveIntegerParser = z.object({
   // TODO 1: 아래와 같이 2개의 양의 정수를 포함한 객체를 parsing하는 zod object를 구현하세요
   // { a: 양의 정수, b: 양의 정수 }
+  // Memo : coerce는 강제 형변환을 진행한다.
+  a: z.coerce.number().int().positive(),
+  b: z.coerce.number().int().positive(),
 });
 
 export const getDbIndexParser = (dbLength: number) =>
   z.object({
+    index: z.coerce.number().int().min(0).max(dbLength-1),
     // db의 크기를 받아 db 범위 내의 인덱스만을 값으로 받는 parser를 만들어 주세요
     // { index: int, 0 <= index <= db_max_index}
+
   });
 
 export const dbPaginationQueryParser = z.object({
@@ -22,4 +30,9 @@ export const dbPaginationQueryParser = z.object({
   //      pageOffset: int, >= 1
   //      itemCount: int, >= 1
   //  },
+
+  startDate: z.optional(z.coerce.date()),
+  endDate: z.optional(z.coerce.date()),
+  pageOffset: z.coerce.number().int().min(1),
+  itemCount: z.coerce.number().int().min(1),
 });
