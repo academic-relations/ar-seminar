89d89f6468c807dd895a6ca1b7d30e1fd759e819
kai._.mjy <kaimjy@kaist.ac.kr>
2024-07-17 20:54:49 +0900

    fix dbElement, twoPositiveIntegerParser, getDbIndexParser, dbPaginationQueryParser

--- 2024-summer-zod-semina/interface-example/db.ts
+++ 2024-summer-zod-semina/interface-example/db.ts
@@ -1,22 +1,27 @@
 import { z } from "zod";
 
 export const dbElement = z.object({
-    value: z.coerce.number().min(0),
-    updatedAt: z.coerce.date(),
- }); 
-
-export const twoPositiveIntegerParser = z.object({
-    a: z.coerce.number().int().min(1),
-    b: z.coerce.number().int().min(1),
+  value: z.coerce.number().min(0),
+  updatedAt: z.coerce.date(),
 });
 
-export const getDbIndexParser = (dbLength: number) => z.object({
-    index: z.coerce.number().int().min(0).max(dbLength - 1),
-})
+export const twoPositiveIntegerParser = z.object({
+  a: z.coerce.number().int().min(1),
+  b: z.coerce.number().int().min(1),
+});
+
+export const getDbIndexParser = (dbLength: number) =>
+  z.object({
+    index: z.coerce
+      .number()
+      .int()
+      .min(0)
+      .max(dbLength - 1),
+  });
 
 export const dbPaginationQueryParser = z.object({
-    startDate: z.optional(z.coerce.date()),
-    endDate: z.optional(z.coerce.date()),
-    pageOffset: z.coerce.number().int().min(1),
-    itemCount: z.coerce.number().int().min(1),
+  startDate: z.optional(z.coerce.date()),
+  endDate: z.optional(z.coerce.date()),
+  pageOffset: z.coerce.number().int().min(1),
+  itemCount: z.coerce.number().int().min(1),
 });
